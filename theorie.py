18b068b55cf191564b5fa4ade6a04feed1157c2c
RobinBenoot2 <100361474+RobinBenoot2@users.noreply.github.com>
2022-08-19 12:18:57 +0200

    Update theorie.py

--- theorie.py
+++ theorie.py
@@ -12,10 +12,12 @@ def printTheorie():
 #-------H1------
 
 def printH1():
-    print('''Variable = general propery of an object, allows to distinguish objects
-    Value = specific propery, interpretation for that variable
-    \u0332".join("Measurement Levels")
+    print(f'''Variable = general propery of an object, allows to distinguish objects
+Value = specific propery, interpretation for that variable
+\u0332".join("Measurement Levels")
 
-    Qualtitative = Limited number of values, not necessarily numeric (bv: small, medium, large)
-    Quantitative = Many values, often unique. Number + unit of measurement (often contain result of measurement)
+Qualtitative = Limited number of values, not necessarily numeric (bv: small, medium, large)
+Quantitative = Many values, often unique. Number + unit of measurement (often contain result of measurement)
     ''')
+
+
