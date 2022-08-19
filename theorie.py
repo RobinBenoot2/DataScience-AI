413e8da3b8a7d9dc248cb1cefc14ac988ef359ac
RobinBenoot2 <100361474+RobinBenoot2@users.noreply.github.com>
2022-08-19 11:22:52 +0200

    Add files via upload
    Init commit theorie

---
+++ theorie.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def help():
+    commands = {'printTheorie()','printH1'}
+
+    for i in commands:
+        print(i)
+
+def printTheorie():
+    printH1()
+
+#-------H1------
+
+def printH1():
+    print(f'Variable = general propery of an object, allows to distinguish objects')
+    print(f'Value = specific propery, interpretation for that variable')
+
+    print(f"\u0332".join("Measurement Levels "))
+
+    print(f'Qualtitative = Limited number of values, not necessarily numeric (bv: small, medium, large)')
+    print(f'Quantitative = Many values, often unique. Number + unit of measurement (often contain result of measurement')
+
+
